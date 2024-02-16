dfdbd0343d7427c64bb57e3b7dcd1489234cce4e
HackPenguin <tdr.vrossi@gmail.com>
2024-02-16 16:34:21 -0500

    añadimos tabla de enrutamiento

--- nuevorepositor.sh
+++ nuevorepositor.sh
@@ -15,7 +15,27 @@ echo "----------"
 
 # display network interfaces and  ip addreses
 
-echo "2.displaying network interfaces an ip addreses
+echo "2.displaying network interfaces an ip addreses"
 ipconfig
 
 echo "-----------"
+
+# display routing table
+
+echo "3.displaying routing table"
+
+netstat -rn
+
+echo "------------"
+
+
+
+#DNS resolution
+
+echo "4. DNS resolution"
+
+nslookup www.google.com
+
+ echo "----------"
+
+ 
